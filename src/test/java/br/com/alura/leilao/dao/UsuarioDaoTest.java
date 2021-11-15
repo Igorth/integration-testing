aeb97e6fbfcf2f3bf3a74a1a54117e9e30313849
Igor Dias <igordias@Igors-MacBook-Pro.local>
2021-11-15 15:57:29 -0400

    added dependecy injection classes dao, autowired

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -5,15 +5,19 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.persistence.EntityManager;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UsuarioDaoTest {
 
     private UsuarioDao dao;
 
+    private EntityManager em;
+
     @Test
     public void test_find_user_by_username() {
-        this.dao = new UsuarioDao();
+        this.dao = new UsuarioDao(em);
         Usuario usuario = dao.buscarPorUsername("fulano");
         assertNotNull(usuario);
     }
